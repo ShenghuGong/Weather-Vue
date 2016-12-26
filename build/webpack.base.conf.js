b6d9c4ef6b89b51487971fd89df32092797fc58b
gsh <gongshenghu@xiaomei.com>
2016-12-26 13:18:52 +0800

    Added copy webpack plugin for webpack

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,6 +2,7 @@ var path = require('path');
 var config = require('../config');
 var utils = require('./utils');
 var projectRoot = path.resolve(__dirname, '../');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 var env = process.env.NODE_ENV;
 // check env & config/index.js to decide whether to enable CSS source maps for the
@@ -80,6 +81,11 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new CopyWebpackPlugin([{
+      from: 'src/favicon.png'
+    }])
+  ],
   eslint: {
     formatter: require('eslint-friendly-formatter')
   },
